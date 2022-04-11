e9e04894e1e8d7e99b7f2aa6ee97eaab71e3286c
Simeon_Klumperbeek <s.g.klumperbeek@gmail.com>
2022-04-11 12:48:52 +0200

    Initial Commit

---
+++ HGB_bot/HGB_bot.js
@@ -0,0 +1,58 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const axios = require('axios');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+// Login to Discord with your client's token
+client.login(token);
+
+// Call api and alternate updating FP with listing every 30 seconds
+const updateFloorPrice = () => {
+  	axios.get('https://api-mainnet.magiceden.dev/v2/collections/honey_genesis_bee/stats') //api call magic eden
+	  .then((res) => {
+		  var floorPrice = res.data.floorPrice/1000000000 
+		  console.log(floorPrice)
+		  client.user?.setPresence({ 
+			status: 'online',
+			activities: [
+				{
+					name: `FP: ${floorPrice} ◎`,
+					type: 'WATCHING'
+				}
+			]
+		})
+	  })
+	  .catch((err) => {
+		  console.log(err);
+	  })
+	setTimeout(updateListed, 1000 * 20) //every 20 seconds 
+}
+
+const updateListed = () => {
+	axios.get('https://api-mainnet.magiceden.dev/v2/collections/honey_genesis_bee/stats') //api call to Magic eden
+	.then((res) => {
+		var listedCount = res.data.listedCount
+		console.log(listedCount)
+	  client.user?.setPresence({ 
+		  status: 'online',
+		  activities: [
+			  {
+				  name: `Listed: ${listedCount}`,
+				  type: 'WATCHING'
+			  }
+		  ]
+	  })
+	})
+	.catch((err) => {
+		console.log(err);
+	})
+  setTimeout(updateFloorPrice, 1000 * 20) //every 20 seconds 
+}
+updateFloorPrice()
---
+++ honey_price_bot/$honey_price_bot.js
@@ -0,0 +1,37 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const axios = require('axios');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+// Login to Discord with your client's token
+client.login(token);
+
+// Call api and update status every 30 seconds
+const updateStatus =  () => {
+  	axios.get('https://public-api.birdeye.so/public/price?address=HonyeYAaTPgKUgQpayL914P6VAqbQZPrbkGMETZvW4iN') //api call to Honey token
+	  .then((res) => {
+		  var value = res.data.data.value.toString().slice(0,8)
+		  console.log(value)
+		  client.user?.setPresence({ 
+			status: 'online',
+			activities: [
+				{
+					name: value,
+					type: 'WATCHING'
+				}
+			]
+		})
+	  })
+	  .catch((err) => {
+		  console.log(err);
+	  })
+	setTimeout(updateStatus, 1000 * 30) //every 30 seconds 
+}
+updateStatus()
